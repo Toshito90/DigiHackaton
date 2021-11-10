c5e15fc9473b1a21d1c498b22e44c2be0c60ccf7
Heitor Silva <heitormonteirosilva90@gmail.com>
2021-11-09 17:36:35 -0800

    - Added NPCs and Interactables

---
+++ DigiEduHack/Assets/Scripts/Interactable/Interactables/Table2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table2 : MonoBehaviour
+{
+	public void Interact()
+	{
+		print("Say Hi to Eduardo.");
+	}
+}
